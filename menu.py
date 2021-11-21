fc95a9909638b0eed424c9cb4cec6d6b0a88106e
lorinho404 <94736865+lorinho404@users.noreply.github.com>
2021-11-20 22:07:46 -0300

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,45 @@
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱   ╭╮╱╭╮╭━━━╮╭╮╱╭╮
+
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱   ┃┃╱┃┃┃╭━╮┃┃┃╱┃┃
+
+╭━━╮╭╮╭╮╭━╮╭━━╮╭━━╮   ┃╰━╯┃┃┃┃┃┃┃╰━╯┃
+
+┃╭━╯┃┃┃┃┃╭╯┃━━┫┃╭╮┃   ╰━━╮┃┃┃┃┃┃╰━━╮┃
+
+┃╰━╮┃╰╯┃┃┃╱┣━━┃┃╰╯┃   ╱╱╱┃┃┃╰━╯┃╱╱╱┃┃
+
+╰━━╯╰━━╯╰╯╱╰━━╯╰━━╯   ╱╱╱╰╯╰━━━╯╱╱╱╰╯
+
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱   ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱
+
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱   ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱
+
+CURSO 404 TERMUX: By LORINHO404
+
+assistencia Mr F
+
+team 404 hacking
+
+yumi-perfeita
+
+TABELA DO CURSO!!
+
+[ 1 ] REDHAWK CURSO TERMUX
+
+[ 2 ] SQLMAP CURSO TERMUX
+
+[ 3 ] DDOS CURSO TERMUX
+
+[ 4 ] COMANDOS INICIAIS CURSO TERMUX
+
+[ 5 ] DOWLOAD PDF CURSO TERMUX
+
+[ 6 ] NOSSO GRUPO WPP CURSO TERMUX
+
+[ 7 ] CURSO 404 VPN
+
+[ 8 ] CURSO 404 COMANDOS EXPLICAÇÃO
+
+[ 9 ] SAIR
+
+'''
